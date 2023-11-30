c63c8d88f029fc7c18c0d6f35280bf5c287e0b3f
Ameerah <ameerah14moos3@gmail.com>
2023-11-30 09:08:25 +0200

    basics of frontend, job, and job type added

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import NavBar from "../components/NavBar";
+import Header from "../components/Header";
+
+const Home = () => {
+  return (
+    <>
+      <NavBar />
+      <Header />
+      <h1>Home page</h1>
+    </>
+  );
+};
+
+export default Home;
