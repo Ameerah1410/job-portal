5dd9ca83790fd8f7cb53869058b359121f5074cb
Ameerah <ameerah14moos3@gmail.com>
2023-12-04 16:06:43 +0200

    added frontend components and styling

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,7 +1,26 @@
+import { Box } from "@mui/material";
 import React from "react";
+import { useTheme } from "@mui/material/styles";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  const { palette } = useTheme();
+  return (
+    <>
+      <Box
+        sx={{
+          height: "70px",
+          bgcolor: palette.secondary.midNightBlue,
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Box component="span" sx={{ color: palette.primary.main }}>
+          <p>© 2023</p>
+        </Box>
+      </Box>
+    </>
+  );
 };
 
 export default Footer;
