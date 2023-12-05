68f6ca294deef6f6aa53e747cf143b0a785d97f8
Ameerah <ameerah14moos3@gmail.com>
2023-12-05 11:47:43 +0200

    added frontend functionality

--- frontend/src/pages/NotFound.js
+++ frontend/src/pages/NotFound.js
@@ -1,9 +1,23 @@
 import React from "react";
+import { Box } from "@mui/material";
+import Footer from "../components/Footer";
+import Navbar from "../components/NavBar";
 
 const NotFound = () => {
   return (
     <>
-      <h1>Page not found</h1>
+      <Navbar />
+      <Box
+        sx={{
+          height: "81vh",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <h1>Page not found!</h1>
+      </Box>
+      <Footer />
     </>
   );
 };
