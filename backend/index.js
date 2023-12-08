bb0f52b7412b1c74dea1c1118bc0db22edefc691
Ameerah Moos <146541029+Ameerah1410@users.noreply.github.com>
2023-12-08 11:57:17 +0200

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -22,13 +22,6 @@ const errorHandler = require("./middleware/error");
 
 const PORT = process.env.PORT || 5000;
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "frontend/build")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-  });
-}
-
 // Initialize cookie session for user authentication
 app.use(
   cookieSession({
