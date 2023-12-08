a5049ba8a70fdbd64b91f580353da8655ac0bfe9
Ameerah Moos <146541029+Ameerah1410@users.noreply.github.com>
2023-12-08 11:38:24 +0200

    Update passport.js

--- backend/passport.js
+++ backend/passport.js
@@ -5,8 +5,8 @@ const passport = require("passport");
 passport.use(
   new GoogleStrategy(
     {
-      clientID: "862658428269-u89t21fnkfa05la8t9qvi6a8ghq05i0i.apps.googleusercontent.com",
-      clientSecret: "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",
+      clientID: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
       callbackURL: "/auth/google/callback", // Callback URL after successful Google authentication
       scope: ["profile", "email"], // Requested user data
     },
